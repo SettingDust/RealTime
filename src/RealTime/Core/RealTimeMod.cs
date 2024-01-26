0f09a2b9f5fc36673031fc1d9ef1b2292b254eba
Tal <talrofeh@gmail.com>
2024-01-26 15:17:57 +0200

    remove logging

--- src/RealTime/Core/RealTimeMod.cs
+++ src/RealTime/Core/RealTimeMod.cs
@@ -44,7 +44,7 @@ namespace RealTime.Core
         /// <summary>Called when this mod is enabled.</summary>
         public void OnEnabled()
         {
-            Log.SetupDebug(Name, LogCategory.Generic, LogCategory.Movement, LogCategory.Simulation, LogCategory.State, LogCategory.Schedule);
+            Log.SetupDebug(Name, LogCategory.Generic);
 
             Log.Info("The 'Real Time' mod has been enabled, version: " + modVersion);
             configProvider = new ConfigurationProvider<RealTimeConfig>(RealTimeConfig.StorageId, Name, () => new RealTimeConfig(latestVersion: true));
