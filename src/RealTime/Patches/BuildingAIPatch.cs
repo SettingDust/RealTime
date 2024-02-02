943ff1fd5b37c0e3aaa85be4fc38a5fdec736193
Tal <talrofeh@gmail.com>
2024-02-02 08:16:22 +0200

    rearrange

--- src/RealTime/Patches/BuildingAIPatch.cs
+++ src/RealTime/Patches/BuildingAIPatch.cs
@@ -2328,6 +2328,187 @@ namespace RealTime.Patches
             }
         }
 
+
+        [HarmonyPatch(typeof(HelicopterDepotAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool HelicopterDepotAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(HospitalAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool HospitalAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(PoliceStationAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool PoliceStationAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(FireStationAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool FireStationAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(DepotAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool DepotAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason reason, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+        [HarmonyPatch(typeof(MaintenanceDepotAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool MaintenanceDepotAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(BankOfficeAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool BankOfficeAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(PostOfficeAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool PostOfficeAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(DisasterResponseBuildingAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool DisasterResponseBuildingAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(CemeteryAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool CemeteryAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(LandfillSiteAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool LandfillSiteAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(FishFarmAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool FishFarmAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(FishingHarborAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool FishingHarborAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        [HarmonyPatch(typeof(SnowDumpAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool SnowDumpAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        [HarmonyPatch(typeof(WaterFacilityAI), "StartTransfer")]
+        [HarmonyPrefix]
+        public static bool WaterFacilityAIStartTransfer(ushort buildingID, ref Building data, TransferManager.TransferReason material, TransferManager.TransferOffer offer)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(CableCarStationAI), "ProduceGoods")]
+        [HarmonyPrefix]
+        public static bool CableCarStationAIProduceGoods(ushort buildingID, ref Building buildingData, ref Building.Frame frameData, int productionRate, int finalProductionRate, ref Citizen.BehaviourData behaviour, int aliveWorkerCount, int totalWorkerCount, int workPlaceCount, int aliveVisitorCount, int totalVisitorCount, int visitPlaceCount)
+        {
+            if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 
 
