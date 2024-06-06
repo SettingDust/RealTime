fd94c17ce9cdde186688ba5dd2a7b4497c252386
Tal <talrofeh@gmail.com>
2024-06-06 07:50:35 +0300

    operation hours of buildings

---
+++ src/RealTime/UI/OperationHoursUIPanel.cs
@@ -0,0 +1,212 @@
+namespace RealTime.UI
+{
+    using ColossalFramework;
+    using ColossalFramework.UI;
+    using RealTime.CustomAI;
+    using UnityEngine;
+
+    public static class OperationHoursUIPanel
+    {
+        public static UIPanel m_uiMainPanel;
+
+        private static CityServiceWorldInfoPanel m_cityServiceWorldInfoPanel;
+
+        private static UILabel m_settingsHeader;
+        private static UILabel m_settingsTitle;
+        private static UICheckBox m_settingsCheckBox;
+
+        private static UICheckBox m_workAtNight;
+        private static UICheckBox m_workAtWeekands;
+        private static UICheckBox m_hasExtendedWorkShift;
+        private static UICheckBox m_hasContinuousWorkShift;
+        private static UISlider m_workShifts;
+
+        private static UIButton SaveBuildingSettingsBtn;
+
+        private static readonly float DEFAULT_HEIGHT = 18F;
+
+        public static void Init() => CreateUI();
+
+        private static void CreateUI()
+        {
+            m_cityServiceWorldInfoPanel = UIView.library.Get<CityServiceWorldInfoPanel>(typeof(CityServiceWorldInfoPanel).Name);
+            var wrapper = m_cityServiceWorldInfoPanel?.Find("Wrapper");
+            var mainSectionPanel = wrapper?.Find("MainSectionPanel");
+            var mainBottom = mainSectionPanel?.Find("MainBottom");
+            var buttonPanels = mainBottom?.Find("ButtonPanels");
+            var m_parkButtons = buttonPanels?.Find("ParkButtons");
+            if (m_parkButtons != null)
+            {
+                m_uiMainPanel = m_cityServiceWorldInfoPanel.component.AddUIComponent<UIPanel>();
+                m_uiMainPanel.name = "OperationHoursUIPanel";
+                m_uiMainPanel.backgroundSprite = "SubcategoriesPanel";
+                m_uiMainPanel.opacity = 0.90f;
+                m_uiMainPanel.isVisible = false;
+                m_uiMainPanel.relativePosition = new Vector3(m_uiMainPanel.parent.width + 1f, 40f);
+                m_uiMainPanel.height = 370f;
+                m_uiMainPanel.width = 510f;
+
+                m_settingsCheckBox = UiUtils.CreateCheckBox(m_parkButtons, "SettingsCheckBox", "settings", false);
+                m_settingsCheckBox.width = 110f;
+                m_settingsCheckBox.label.textColor = new Color32(185, 221, 254, 255);
+                m_settingsCheckBox.label.textScale = 0.8125f;
+                m_settingsCheckBox.tooltip = "change building operation hours.";
+                m_settingsCheckBox.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
+                m_settingsCheckBox.relativePosition = new Vector3(m_uiMainPanel.width - m_settingsCheckBox.width, 6f);
+                m_settingsCheckBox.eventCheckChanged += (component, value) =>
+                {
+                    m_uiMainPanel.isVisible = value;
+                    m_uiMainPanel.height = m_uiMainPanel.parent.height - 7f;
+                };
+                m_parkButtons.AttachUIComponent(m_settingsCheckBox.gameObject);
+
+                m_settingsHeader = UiUtils.CreateLabel(m_uiMainPanel, "SettingsPanelHeader", "Settings", "");
+                m_settingsHeader.font = UiUtils.GetUIFont("OpenSans-Regular");
+                m_settingsHeader.textAlignment = UIHorizontalAlignment.Center;
+                m_settingsHeader.relativePosition = new Vector3(10f, 60f + 0 * (DEFAULT_HEIGHT * 0.8f + 2f));
+
+                m_settingsTitle = UiUtils.CreateLabel(m_uiMainPanel, "SettingsTitle", "Shift Settings", "");
+                m_settingsTitle.font = UiUtils.GetUIFont("OpenSans-Regular");
+                m_settingsTitle.textAlignment = UIHorizontalAlignment.Center;
+                m_settingsTitle.textColor = new Color32(215, 51, 58, 255);
+                m_settingsTitle.relativePosition = new Vector3(100f, 20f);
+                m_settingsTitle.textScale = 1.2f;
+
+                m_workAtNight = UiUtils.CreateCheckBox(m_uiMainPanel, "WorkAtNight", "Work At Night", false);
+                m_workAtNight.width = 110f;
+                m_workAtNight.label.textColor = new Color32(185, 221, 254, 255);
+                m_workAtNight.label.textScale = 0.8125f;
+                m_workAtNight.tooltip = "choose if the building will work at night.";
+                m_workAtNight.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
+                m_workAtNight.relativePosition = new Vector3(m_uiMainPanel.width - m_workAtNight.width, 6f);
+                m_workAtNight.eventCheckChanged += (component, value) =>
+                {
+                    m_workAtNight.isChecked = value;
+                    UpdateSlider();
+                };
+
+                m_workAtWeekands = UiUtils.CreateCheckBox(m_uiMainPanel, "WorkAtWeekands", "Work At Weekands", false);
+                m_workAtWeekands.width = 110f;
+                m_workAtWeekands.label.textColor = new Color32(185, 221, 254, 255);
+                m_workAtWeekands.label.textScale = 0.8125f;
+                m_workAtWeekands.tooltip = "choose if the building will work at weekends.";
+                m_workAtWeekands.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
+                m_workAtWeekands.relativePosition = new Vector3(m_uiMainPanel.width - m_workAtWeekands.width, 6f);
+                m_workAtWeekands.eventCheckChanged += (component, value) => m_workAtWeekands.isChecked = value;
+
+                m_hasExtendedWorkShift = UiUtils.CreateCheckBox(m_uiMainPanel, "HasExtendedWorkShift", "Has Extended Work Shift", false);
+                m_hasExtendedWorkShift.width = 110f;
+                m_hasExtendedWorkShift.label.textColor = new Color32(185, 221, 254, 255);
+                m_hasExtendedWorkShift.label.textScale = 0.8125f;
+                m_hasExtendedWorkShift.tooltip = "choose if the building will have an extended work shift.";
+                m_hasExtendedWorkShift.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
+                m_hasExtendedWorkShift.relativePosition = new Vector3(m_uiMainPanel.width - m_hasExtendedWorkShift.width, 6f);
+                m_hasExtendedWorkShift.eventCheckChanged += (component, value) =>
+                {
+                    m_hasExtendedWorkShift.isChecked = value;
+                    UpdateSlider();
+                };
+
+                m_hasContinuousWorkShift = UiUtils.CreateCheckBox(m_uiMainPanel, "HasContinuousWorkShift", "Has Continuous Work Shift", false);
+                m_hasContinuousWorkShift.width = 110f;
+                m_hasContinuousWorkShift.label.textColor = new Color32(185, 221, 254, 255);
+                m_hasContinuousWorkShift.label.textScale = 0.8125f;
+                m_hasContinuousWorkShift.tooltip = "choose if the building will have a continuous work shift.";
+                m_hasContinuousWorkShift.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
+                m_hasContinuousWorkShift.relativePosition = new Vector3(m_uiMainPanel.width - m_hasContinuousWorkShift.width, 6f);
+                m_hasContinuousWorkShift.eventCheckChanged += (component, value) =>
+                {
+                    m_hasContinuousWorkShift.isChecked = value;
+                    UpdateSlider();
+                };
+
+                m_workShifts = UiUtils.CreateSlider(m_uiMainPanel, "ShiftCount", 1, 3, 1, 1);
+                m_workShifts.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
+                m_workShifts.relativePosition = new Vector3(m_uiMainPanel.width - m_workShifts.width, 6f);
+
+                SaveBuildingSettingsBtn = UiUtils.AddButton(m_uiMainPanel, 260f, 60f + 2 * (DEFAULT_HEIGHT * 0.8f + 2f), "SaveBuildingSettings", "save building settings", "save building working hours");
+                SaveBuildingSettingsBtn.eventClicked += SaveBuildingSettings;
+            }
+        }
+
+        public static void UpdateSlider()
+        {
+            if(m_hasExtendedWorkShift.isChecked)
+            {
+                m_hasContinuousWorkShift.isChecked = false;
+            }
+            if (m_hasContinuousWorkShift.isChecked)
+            {
+                m_workShifts.maxValue = m_workAtNight.isChecked ? 2 : 1;
+                m_hasExtendedWorkShift.isChecked = false;
+            }
+            else
+            {
+                m_workShifts.maxValue = m_workAtNight.isChecked ? 3 : 2;
+            }
+        }
+
+        public static void RefreshData()
+        {
+            ushort buildingID = WorldInfoPanel.GetCurrentInstanceID().Building;
+            var building = Singleton<BuildingManager>.instance.m_buildings.m_buffer[buildingID];
+            var buildingAI = building.Info.GetAI();
+            if (buildingAI is not CommercialBuildingAI)
+            {
+                m_settingsCheckBox.Hide();
+                m_uiMainPanel.Hide();
+            }
+            else
+            {
+                if (BuildingWorkTimeManager.BuildingsWorkTime.TryGetValue(buildingID, out var buildingWorkTime))
+                {
+                    m_workAtNight.isChecked = buildingWorkTime.WorkAtNight;
+                    m_workAtWeekands.isChecked = buildingWorkTime.WorkAtWeekands;
+                    m_hasExtendedWorkShift.isChecked = buildingWorkTime.HasExtendedWorkShift;
+                    m_hasContinuousWorkShift.isChecked = buildingWorkTime.HasContinuousWorkShift;
+                    m_workShifts.value = buildingWorkTime.WorkShifts;
+                }
+                m_settingsCheckBox.Show();
+                if (m_settingsCheckBox.isChecked)
+                {
+                    m_uiMainPanel.height = 370f;
+                    m_uiMainPanel.Show();
+                }
+            }
+        }
+
+        public static void SaveBuildingSettings(UIComponent c, UIMouseEventParameter eventParameter) => SaveSettings();
+
+        private static void SaveSettings()
+        {
+            ushort buildingID = WorldInfoPanel.GetCurrentInstanceID().Building;
+
+            var buildingWorkTime = BuildingWorkTimeManager.GetBuildingWorkTime(buildingID);
+
+            buildingWorkTime.WorkAtNight = m_workAtNight;
+            buildingWorkTime.WorkAtWeekands = m_workAtWeekands.isChecked;
+            buildingWorkTime.HasExtendedWorkShift = m_hasExtendedWorkShift.isChecked;
+            buildingWorkTime.HasContinuousWorkShift = m_hasContinuousWorkShift.isChecked;
+            buildingWorkTime.WorkShifts = (int)m_workShifts.value;
+
+            BuildingWorkTimeManager.SetBuildingWorkTime(buildingID, buildingWorkTime);
+
+            RefreshData();
+        }
+
+        public static void LoadSettings(ushort buildingID)
+        {
+
+            if (BuildingWorkTimeManager.BuildingsWorkTime.TryGetValue(buildingID, out var buildingWorkTime))
+            {
+                m_workAtNight.isChecked = buildingWorkTime.WorkAtNight;
+                m_workAtWeekands.isChecked = buildingWorkTime.WorkAtWeekands;
+                m_hasExtendedWorkShift.isChecked = buildingWorkTime.HasExtendedWorkShift;
+                m_hasContinuousWorkShift.isChecked = buildingWorkTime.HasContinuousWorkShift;
+                m_workShifts.value = buildingWorkTime.WorkShifts;
+            }
+
+        }
+    }
+
+}
---
+++ src/RealTime/UI/UiUtils.cs
@@ -0,0 +1,187 @@
+namespace RealTime.UI
+{
+    using ColossalFramework.UI;
+    using UnityEngine;
+
+    public static class UiUtils
+    {
+        public static UIButton AddButton(UIComponent parent, float posX, float posY, string name, string text, string tooltip = null, float width = 230f, float height = 30f, float scale = 0.9f, int vertPad = 4)
+        {
+            var button = parent.AddUIComponent<UIButton>();
+
+            // Size and position.
+            button.size = new Vector2(width, height);
+            button.relativePosition = new Vector2(posX, posY);
+
+            // Appearance.
+            button.textScale = scale;
+            button.normalBgSprite = "ButtonWhite";
+            button.hoveredBgSprite = "ButtonWhite";
+            button.focusedBgSprite = "ButtonWhite";
+            button.pressedBgSprite = "ButtonWhitePressed";
+            button.disabledBgSprite = "ButtonWhiteDisabled";
+            button.color = Color.white;
+            button.focusedColor = Color.white;
+            button.hoveredColor = Color.white;
+            button.pressedColor = Color.white;
+            button.textColor = Color.black;
+            button.pressedTextColor = Color.black;
+            button.focusedTextColor = Color.black;
+            button.hoveredTextColor = Color.blue;
+            button.disabledTextColor = Color.grey;
+            button.canFocus = false;
+
+            // Add tooltip.
+            if (tooltip != null)
+            {
+                button.tooltip = tooltip;
+            }
+
+            // Text.
+            button.textScale = scale;
+            button.textPadding = new RectOffset(0, 0, vertPad, 0);
+            button.textVerticalAlignment = UIVerticalAlignment.Middle;
+            button.textHorizontalAlignment = UIHorizontalAlignment.Center;
+            button.text = text;
+            button.name = name;
+
+            return button;
+        }
+
+        public static UICheckBox CreateCheckBox(UIComponent parent, string name, string text, bool state)
+        {
+            var checkBox = parent.AddUIComponent<UICheckBox>();
+            checkBox.name = name;
+
+            checkBox.height = 16f;
+            checkBox.width = parent.width - 10f;
+
+            var uncheckedSprite = checkBox.AddUIComponent<UISprite>();
+            uncheckedSprite.spriteName = "check-unchecked";
+            uncheckedSprite.size = new Vector2(16f, 16f);
+            uncheckedSprite.relativePosition = Vector3.zero;
+
+            var checkedSprite = checkBox.AddUIComponent<UISprite>();
+            checkedSprite.spriteName = "check-checked";
+            checkedSprite.size = new Vector2(16f, 16f);
+            checkedSprite.relativePosition = Vector3.zero;
+            checkBox.checkedBoxObject = checkedSprite;
+
+            checkBox.label = checkBox.AddUIComponent<UILabel>();
+            checkBox.label.text = text;
+            checkBox.label.font = GetUIFont("OpenSans-Regular");
+            checkBox.label.autoSize = false;
+            checkBox.label.height = 20f;
+            checkBox.label.verticalAlignment = UIVerticalAlignment.Middle;
+            checkBox.label.relativePosition = new Vector3(20f, 0f);
+
+            checkBox.isChecked = state;
+
+            return checkBox;
+        }
+
+        public static UIFont GetUIFont(string name)
+        {
+            var fonts = Resources.FindObjectsOfTypeAll<UIFont>();
+
+            foreach (var font in fonts)
+            {
+                if (font.name.CompareTo(name) == 0)
+                {
+                    return font;
+                }
+            }
+
+            return null;
+        }
+
+        public static UILabel CreateLabel(UIComponent parent, string name, string text, string prefix)
+        {
+            var label = parent.AddUIComponent<UILabel>();
+            label.name = name;
+            label.text = text;
+            label.prefix = prefix;
+
+            return label;
+        }
+
+        public static UIPanel CreatePanel(UIComponent parent, string name)
+        {
+            var panel = parent.AddUIComponent<UIPanel>();
+            panel.name = name;
+
+            return panel;
+        }
+
+        public static UITextField CreateTextField(UIComponent parent, string name, string tooltip)
+        {
+            var textField = parent.AddUIComponent<UITextField>();
+            textField.name = name;
+            textField.padding = new RectOffset(0, 0, 9, 3);
+            textField.builtinKeyNavigation = true;
+            textField.isInteractive = true;
+            textField.readOnly = false;
+            textField.horizontalAlignment = UIHorizontalAlignment.Center;
+            textField.verticalAlignment = UIVerticalAlignment.Middle;
+            textField.selectionSprite = "EmptySprite";
+            textField.selectionBackgroundColor = new Color32(233, 201, 148, 255);
+            textField.normalBgSprite = "TextFieldPanelHovered";
+            textField.disabledBgSprite = "TextFieldPanel";
+            textField.textColor = new Color32(0, 0, 0, 255);
+            textField.disabledTextColor = new Color32(0, 0, 0, 128);
+            textField.color = new Color32(185, 221, 254, 255);
+            textField.tooltip = tooltip;
+            textField.size = new Vector2(50f, 27f);
+            textField.padding.top = 6;
+            textField.numericalOnly = true;
+            textField.allowNegative = false;
+            textField.allowFloats = false;
+            textField.multiline = false;
+            textField.textScale = 1.0f;
+
+            return textField;
+        }
+
+        public static Vector3 PositionUnder(UIComponent uIComponent, float margin = 8f, float horizontalOffset = 0f) => new Vector3(uIComponent.relativePosition.x + horizontalOffset, uIComponent.relativePosition.y + uIComponent.height + margin);
+
+        public static Vector3 PositionRightOf(UIComponent uIComponent, float margin = 8f, float verticalOffset = 0f) => new Vector3(uIComponent.relativePosition.x + uIComponent.width + margin, uIComponent.relativePosition.y + verticalOffset);
+
+        public static UIPanel UIServiceBar(UIComponent parent, string name, string text, string prefix, string tooltip)
+        {
+            float DEFAULT_SCALE = 0.8f;
+            // panel
+            var m_uiPanel = parent.AddUIComponent<UIPanel>();
+            m_uiPanel.name = name;
+            m_uiPanel.height = 20f;
+            m_uiPanel.width = 440f;
+
+            // text
+            string label_name = name + "Label";
+            var m_uiTextLabel = CreateLabel(m_uiPanel, label_name, text, prefix);
+            m_uiTextLabel.textAlignment = UIHorizontalAlignment.Left;
+            m_uiTextLabel.relativePosition = new Vector3(0, 0);
+            m_uiTextLabel.textScale = DEFAULT_SCALE;
+
+            // value
+            string text_name = name + "TextField";
+            var m_uiValueLabel = CreateTextField(m_uiPanel, text_name, tooltip);
+            m_uiValueLabel.relativePosition = new Vector3(180f, -6f);
+
+            return m_uiPanel;
+        }
+
+        public static UISlider CreateSlider(UIComponent parent, string name, float min, float max, float step, float defaultValue)
+        {
+            var slider = parent.AddUIComponent<UISlider>();
+            slider.name = name;
+            slider.relativePosition = Vector3.zero;
+            slider.backgroundSprite = "ScrollbarTrack";
+            slider.size = new Vector2(171, 12);
+            slider.value = defaultValue;
+            slider.minValue = min;
+            slider.maxValue = max;
+            slider.stepSize = step;
+            return slider;
+        }
+    }
+}
