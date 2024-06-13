f58135ba7b7652b9bac5f9c9fd595b83c54dde81
Tal <talrofeh@gmail.com>
2024-06-14 02:05:53 +0300

    update checkbox position

--- src/RealTime/UI/CityServiceOperationHoursUIPanel.cs
+++ src/RealTime/UI/CityServiceOperationHoursUIPanel.cs
@@ -52,7 +52,7 @@ namespace RealTime.UI
                 m_operationHoursSettingsCheckBox.label.textScale = 0.8125f;
                 m_operationHoursSettingsCheckBox.tooltip = "change building operation hours.";
                 m_operationHoursSettingsCheckBox.AlignTo(m_cityServiceWorldInfoPanel.component, UIAlignAnchor.TopLeft);
-                m_operationHoursSettingsCheckBox.relativePosition = new Vector3(350f, 6f);
+                m_operationHoursSettingsCheckBox.relativePosition = new Vector3(400f, 26f);
                 m_operationHoursSettingsCheckBox.eventCheckChanged += (component, value) =>
                 {
                     m_uiMainPanel.isVisible = value;
