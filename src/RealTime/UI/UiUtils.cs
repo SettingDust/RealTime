098d7f97bae5b0147b6c715c337dd8094cc1ec19
Tal <talrofeh@gmail.com>
2024-06-14 01:59:10 +0300

    add an extended create label

--- src/RealTime/UI/UiUtils.cs
+++ src/RealTime/UI/UiUtils.cs
@@ -105,6 +105,41 @@ namespace RealTime.UI
             return label;
         }
 
+        public static UILabel CreateLabel(UIComponent parent, float xPos, float yPos, string text, float width = -1f, float textScale = 1.0f, UIHorizontalAlignment alignment = UIHorizontalAlignment.Left)
+        {
+            // Add label.
+            var label = parent.AddUIComponent<UILabel>();
+
+            // Set sizing options.
+            if (width > 0f)
+            {
+                // Fixed width.
+                label.autoSize = false;
+                label.width = width;
+                label.autoHeight = true;
+                label.wordWrap = true;
+            }
+            else
+            {
+                // Autosize.
+                label.autoSize = true;
+                label.autoHeight = false;
+                label.wordWrap = false;
+            }
+
+            // Alignment.
+            label.textAlignment = alignment;
+
+            // Text.
+            label.textScale = textScale;
+            label.text = text;
+
+            // Position (aligned to right if text alignment is set to right).
+            label.relativePosition = new Vector2(alignment == UIHorizontalAlignment.Right ? xPos - label.width : xPos, yPos);
+
+            return label;
+        }
+
         public static UIPanel CreatePanel(UIComponent parent, string name)
         {
             var panel = parent.AddUIComponent<UIPanel>();
