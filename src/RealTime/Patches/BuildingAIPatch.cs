0bd186144fd2efbec045e1c9ff466b42ca3c175e
Tal <talrofeh@gmail.com>
2024-06-22 19:09:25 +0300

    fix patches

--- src/RealTime/Patches/BuildingAIPatch.cs
+++ src/RealTime/Patches/BuildingAIPatch.cs
@@ -43,36 +43,44 @@ namespace RealTime.Patches
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> TranspileSimulationStepActive(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
+                var IsBuildingWorkingInstance = AccessTools.PropertyGetter(typeof(BuildingAIPatch), nameof(RealTimeBuildingAI));
                 var IsBuildingWorking = typeof(RealTimeBuildingAI).GetMethod("IsBuildingWorking", BindingFlags.Public | BindingFlags.Instance);
                 var inst = new List<CodeInstruction>(instructions);
-                var jumpLabel = generator.DefineLabel();
 
                 for (int i = 0; i < inst.Count; i++)
                 {
-                    if (inst[i].opcode == OpCodes.Ldfld && (inst[i].operand as int?) == (int)Building.Flags.Evacuating && inst[i + 1].opcode == OpCodes.Ldarg_2)
+                    if (inst[i].opcode == OpCodes.Ldfld &&
+                        inst[i].operand == typeof(Building).GetField("m_flags") &&
+                        inst[i + 1].opcode == OpCodes.Ldc_I4 &&
+                        inst[i + 1].operand is int s &&
+                        s == 32768 &&
+                        inst[i - 1].opcode == OpCodes.Ldarg_2)
                     {
-                        inst[i-1].labels.Add(jumpLabel);
                         inst.InsertRange(i - 1, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
-                            new(OpCodes.Ldc_I4_0),
-                            new(OpCodes.Stloc_S, 10)
-                        ]); 
-                    }
-
-                    if (inst[i].opcode == OpCodes.Ldfld && inst[i].operand == typeof(Building).GetField("m_fireIntensity"))
-                    {
-                        inst[i - 1].labels.Add(jumpLabel);
-                        inst.InsertRange(i - 1, [
-                            new(OpCodes.Ldarg_1),
-                            new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
+                            new(OpCodes.Brfalse, inst[i + 3].operand),
                             new(OpCodes.Ldc_I4_0),
                             new(OpCodes.Stloc_S, 10)
                         ]);
+                        break;
                     }
                 }
+
+                for (int i = 0; i < inst.Count; i++)
+                {
+                    if (inst[i].opcode == OpCodes.Ldfld && inst[i].operand == typeof(Building).GetField("m_fireIntensity"))
+                    {
+                        inst.InsertRange(i + 2, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
+                            new(OpCodes.Ldarg_1),
+                            new(OpCodes.Call, IsBuildingWorking),
+                            new(OpCodes.Brtrue, inst[i + 1].operand)
+                        ]);
+                    }
+                }
+
                 return inst;
             }
 
@@ -134,42 +142,6 @@ namespace RealTime.Patches
                 }
             }
 
-            [HarmonyPatch]
-            private sealed class PrivateBuildingAI_SimulationStep
-            {
-                private delegate void EmptyBuildingDelegate(CommonBuildingAI __instance, ushort buildingID, ref Building data, CitizenUnit.Flags flags, bool onlyMoving);
-                private static readonly EmptyBuildingDelegate EmptyBuilding = AccessTools.MethodDelegate<EmptyBuildingDelegate>(typeof(CommonBuildingAI).GetMethod("EmptyBuilding", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
-
-                [HarmonyPatch(typeof(PrivateBuildingAI), "SimulationStep")]
-                [HarmonyPostfix]
-                private static void Postfix(PrivateBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData)
-                {
-                    if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
-                    {
-                        buildingData.m_flags &= ~Building.Flags.EventActive;
-                        EmptyBuilding(__instance, buildingID, ref buildingData, CitizenUnit.Flags.Created, onlyMoving: false);
-                    }
-                }
-            }
-
-            [HarmonyPatch]
-            private sealed class PlayerBuildingAI_SimulationStep
-            {
-                private delegate void EmptyBuildingDelegate(CommonBuildingAI __instance, ushort buildingID, ref Building data, CitizenUnit.Flags flags, bool onlyMoving);
-                private static readonly EmptyBuildingDelegate EmptyBuilding = AccessTools.MethodDelegate<EmptyBuildingDelegate>(typeof(CommonBuildingAI).GetMethod("EmptyBuilding", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
-
-                [HarmonyPatch(typeof(PlayerBuildingAI), "SimulationStep")]
-                [HarmonyPostfix]
-                private static void Postfix(PrivateBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData)
-                {
-                    if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
-                    {
-                        buildingData.m_flags &= ~Building.Flags.EventActive;
-                        EmptyBuilding(__instance, buildingID, ref buildingData, CitizenUnit.Flags.Created, onlyMoving: false);
-                    }
-                }
-            }
-
             private static void GetHotelBehaviour(ushort buildingID, ref Building buildingData, ref Citizen.BehaviourData behaviour, ref int aliveCount, ref int totalCount)
             {
                 var instance = Singleton<CitizenManager>.instance;
@@ -198,36 +170,44 @@ namespace RealTime.Patches
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> TranspileSimulationStepActive(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
+                var IsBuildingWorkingInstance = AccessTools.PropertyGetter(typeof(BuildingAIPatch), nameof(RealTimeBuildingAI));
                 var IsBuildingWorking = typeof(RealTimeBuildingAI).GetMethod("IsBuildingWorking", BindingFlags.Public | BindingFlags.Instance);
                 var inst = new List<CodeInstruction>(instructions);
-                var jumpLabel = generator.DefineLabel();
 
                 for (int i = 0; i < inst.Count; i++)
                 {
-                    if (inst[i].opcode == OpCodes.Ldfld && (inst[i].operand as int?) == (int)Building.Flags.Evacuating && inst[i + 1].opcode == OpCodes.Ldarg_2)
+                    if (inst[i].opcode == OpCodes.Ldfld &&
+                        inst[i].operand == typeof(Building).GetField("m_flags") &&
+                        inst[i + 1].opcode == OpCodes.Ldc_I4 &&
+                        inst[i + 1].operand is int s &&
+                        s == 32768 &&
+                        inst[i - 1].opcode == OpCodes.Ldarg_2)
                     {
-                        inst[i - 1].labels.Add(jumpLabel);
                         inst.InsertRange(i - 1, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
+                            new(OpCodes.Brfalse, inst[i + 3].operand),
                             new(OpCodes.Ldc_I4_0),
                             new(OpCodes.Stloc_S, 10)
                         ]);
+                        break;
                     }
+                }
 
+                for (int i = 0; i < inst.Count; i++)
+                {
                     if (inst[i].opcode == OpCodes.Ldfld && inst[i].operand == typeof(Building).GetField("m_fireIntensity"))
                     {
-                        inst[i - 1].labels.Add(jumpLabel);
-                        inst.InsertRange(i - 1, [
+                        inst.InsertRange(i + 2, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
-                            new(OpCodes.Ldc_I4_0),
-                            new(OpCodes.Stloc_S, 10)
+                            new(OpCodes.Brtrue, inst[i + 1].operand)
                         ]);
                     }
                 }
+
                 return inst;
             }
         }
@@ -239,36 +219,44 @@ namespace RealTime.Patches
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> TranspileSimulationStepActive(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
+                var IsBuildingWorkingInstance = AccessTools.PropertyGetter(typeof(BuildingAIPatch), nameof(RealTimeBuildingAI));
                 var IsBuildingWorking = typeof(RealTimeBuildingAI).GetMethod("IsBuildingWorking", BindingFlags.Public | BindingFlags.Instance);
                 var inst = new List<CodeInstruction>(instructions);
-                var jumpLabel = generator.DefineLabel();
 
                 for (int i = 0; i < inst.Count; i++)
                 {
-                    if (inst[i].opcode == OpCodes.Ldfld && (inst[i].operand as int?) == (int)Building.Flags.Evacuating && inst[i + 1].opcode == OpCodes.Ldarg_2)
+                    if (inst[i].opcode == OpCodes.Ldfld &&
+                        inst[i].operand == typeof(Building).GetField("m_flags") &&
+                        inst[i + 1].opcode == OpCodes.Ldc_I4 &&
+                        inst[i + 1].operand is int s &&
+                        s == 32768 &&
+                        inst[i - 1].opcode == OpCodes.Ldarg_2)
                     {
-                        inst[i - 1].labels.Add(jumpLabel);
                         inst.InsertRange(i - 1, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
+                            new(OpCodes.Brfalse, inst[i + 3].operand),
                             new(OpCodes.Ldc_I4_0),
                             new(OpCodes.Stloc_S, 10)
                         ]);
+                        break;
                     }
+                }
 
+                for (int i = 0; i < inst.Count; i++)
+                {
                     if (inst[i].opcode == OpCodes.Ldfld && inst[i].operand == typeof(Building).GetField("m_fireIntensity"))
                     {
-                        inst[i - 1].labels.Add(jumpLabel);
-                        inst.InsertRange(i - 1, [
+                        inst.InsertRange(i + 2, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
-                            new(OpCodes.Ldc_I4_0),
-                            new(OpCodes.Stloc_S, 10)
+                            new(OpCodes.Brtrue, inst[i + 1].operand)
                         ]);
                     }
                 }
+
                 return inst;
             }
         }
@@ -280,37 +268,81 @@ namespace RealTime.Patches
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> TranspileSimulationStepActive(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
+                var IsBuildingWorkingInstance = AccessTools.PropertyGetter(typeof(BuildingAIPatch), nameof(RealTimeBuildingAI));
                 var IsBuildingWorking = typeof(RealTimeBuildingAI).GetMethod("IsBuildingWorking", BindingFlags.Public | BindingFlags.Instance);
                 var inst = new List<CodeInstruction>(instructions);
-                var jumpLabel = generator.DefineLabel();
 
                 for (int i = 0; i < inst.Count; i++)
                 {
-                    if (inst[i].opcode == OpCodes.Ldfld && (inst[i].operand as int?) == (int)Building.Flags.Evacuating && inst[i + 1].opcode == OpCodes.Ldarg_2)
+                    if (inst[i].opcode == OpCodes.Ldfld &&
+                        inst[i].operand == typeof(Building).GetField("m_flags") &&
+                        inst[i + 1].opcode == OpCodes.Ldc_I4 &&
+                        inst[i + 1].operand is int s &&
+                        s == 32768 &&
+                        inst[i - 1].opcode == OpCodes.Ldarg_2)
                     {
-                        inst[i - 1].labels.Add(jumpLabel);
                         inst.InsertRange(i - 1, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
+                            new(OpCodes.Brfalse, inst[i + 3].operand),
                             new(OpCodes.Ldc_I4_0),
                             new(OpCodes.Stloc_S, 10)
                         ]);
+                        break;
                     }
+                }
 
+                for (int i = 0; i < inst.Count; i++)
+                {
                     if (inst[i].opcode == OpCodes.Ldfld && inst[i].operand == typeof(Building).GetField("m_fireIntensity"))
                     {
-                        inst[i - 1].labels.Add(jumpLabel);
-                        inst.InsertRange(i - 1, [
+                        inst.InsertRange(i + 2, [
+                            new(OpCodes.Call, IsBuildingWorkingInstance),
                             new(OpCodes.Ldarg_1),
                             new(OpCodes.Call, IsBuildingWorking),
-                            new(OpCodes.Brfalse, jumpLabel),
-                            new(OpCodes.Ldc_I4_0),
-                            new(OpCodes.Stloc_S, 10)
+                            new(OpCodes.Brtrue, inst[i + 1].operand)
                         ]);
                     }
                 }
+
                 return inst;
+            }
+        }
+
+        [HarmonyPatch]
+        private sealed class PrivateBuildingAI_SimulationStep
+        {
+            private delegate void EmptyBuildingDelegate(CommonBuildingAI __instance, ushort buildingID, ref Building data, CitizenUnit.Flags flags, bool onlyMoving);
+            private static readonly EmptyBuildingDelegate EmptyBuilding = AccessTools.MethodDelegate<EmptyBuildingDelegate>(typeof(CommonBuildingAI).GetMethod("EmptyBuilding", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+            [HarmonyPatch(typeof(PrivateBuildingAI), "SimulationStep")]
+            [HarmonyPostfix]
+            private static void Postfix(PrivateBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData)
+            {
+                if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+                {
+                    buildingData.m_flags &= ~Building.Flags.EventActive;
+                    EmptyBuilding(__instance, buildingID, ref buildingData, CitizenUnit.Flags.Created, onlyMoving: false);
+                }
+            }
+        }
+
+        [HarmonyPatch]
+        private sealed class PlayerBuildingAI_SimulationStep
+        {
+            private delegate void EmptyBuildingDelegate(CommonBuildingAI __instance, ushort buildingID, ref Building data, CitizenUnit.Flags flags, bool onlyMoving);
+            private static readonly EmptyBuildingDelegate EmptyBuilding = AccessTools.MethodDelegate<EmptyBuildingDelegate>(typeof(CommonBuildingAI).GetMethod("EmptyBuilding", BindingFlags.Instance | BindingFlags.NonPublic), null, false);
+
+            [HarmonyPatch(typeof(PlayerBuildingAI), "SimulationStep")]
+            [HarmonyPostfix]
+            private static void Postfix(PrivateBuildingAI __instance, ushort buildingID, ref Building buildingData, ref Building.Frame frameData)
+            {
+                if (!RealTimeBuildingAI.IsBuildingWorking(buildingID))
+                {
+                    buildingData.m_flags &= ~Building.Flags.EventActive;
+                    EmptyBuilding(__instance, buildingID, ref buildingData, CitizenUnit.Flags.Created, onlyMoving: false);
+                }
             }
         }
 
