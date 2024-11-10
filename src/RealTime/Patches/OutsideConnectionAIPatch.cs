0654483c8f340ed232b7dd44b48bca22cd56e5df
Tal <talrofeh@gmail.com>
2024-11-10 17:08:49 +0200

    remove extra outside connections

--- src/RealTime/Patches/OutsideConnectionAIPatch.cs
+++ src/RealTime/Patches/OutsideConnectionAIPatch.cs
@@ -2,8 +2,6 @@
 
 namespace RealTime.Patches
 {
-    using System.Reflection;
-    using ColossalFramework;
     using HarmonyLib;
     using RealTime.CustomAI;
 
@@ -30,46 +28,5 @@ namespace RealTime.Patches
             }
         }
 
-        [HarmonyPatch]
-        private sealed class OutsideConnectionAI_SimulationStep
-        {
-            [HarmonyPatch(typeof(OutsideConnectionAI), "SimulationStep")]
-            [HarmonyPostfix]
-            private static void Postfix(ushort buildingID, ref Building data)
-            {
-                if ((Singleton<ToolManager>.instance.m_properties.m_mode & ItemClass.Availability.Game) != 0)
-                {
-                    var instance = Singleton<SimulationManager>.instance;
-                    var instance2 = Singleton<TransferManager>.instance;
-                    var offer = default(TransferManager.TransferOffer);
-                    offer.Building = buildingID;
-                    offer.Unlimited = true;
-                    offer.Position = data.m_position * (instance.m_randomizer.Int32(100, 400) * 0.01f);
-                    offer.Active = true;
-                    offer.Amount = 1;
-                    offer.Priority = instance.m_randomizer.Int32(8u);
-                    int[] m_incomingAmount = (int[])typeof(TransferManager).GetField("m_incomingAmount", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(instance2);
-                    int[] m_outgoingAmount = (int[])typeof(TransferManager).GetField("m_outgoingAmount", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(instance2);
-                    int leave_city0_in = m_incomingAmount[(int)TransferManager.TransferReason.LeaveCity0];
-                    int leave_city1_in = m_incomingAmount[(int)TransferManager.TransferReason.LeaveCity1];
-                    int leave_city2_in = m_incomingAmount[(int)TransferManager.TransferReason.LeaveCity2];
-                    int leave_city0_out = m_outgoingAmount[(int)TransferManager.TransferReason.LeaveCity0];
-                    int leave_city1_out = m_outgoingAmount[(int)TransferManager.TransferReason.LeaveCity1];
-                    int leave_city2_out = m_outgoingAmount[(int)TransferManager.TransferReason.LeaveCity2];
-                    if (leave_city0_out < leave_city0_in)
-                    {
-                        instance2.AddOutgoingOffer(TransferManager.TransferReason.LeaveCity0, offer);
-                    }
-                    if (leave_city1_out < leave_city1_in)
-                    {
-                        instance2.AddOutgoingOffer(TransferManager.TransferReason.LeaveCity1, offer);
-                    }
-                    if (leave_city2_out < leave_city2_in)
-                    {
-                        instance2.AddOutgoingOffer(TransferManager.TransferReason.LeaveCity2, offer);
-                    }
-                }
-            }
-        }
     }
 }
